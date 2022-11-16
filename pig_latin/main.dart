2e8a44d2ad897d77045d63d2612c4a811f6f81a3
Erick Ogaro <ogaroerick1998@gmail.com>
2022-11-16 10:54:58 +0300

    piglatin

--- pig_latin/main.dart
+++ pig_latin/main.dart
@@ -1,15 +1,13 @@
-void main(List<String> args) {
-  String sentence = "The quick brown fox jumps over the lazy dog";
-  print(pigLatin(sentence));
-}
-
 String pigLatin(String sentence) {
   List<String> words = sentence.split(" ");
   String pigLatin = "";
   for (var i = 0; i < words.length; i++) {
-    String currentWordReversed = words[i].split("").reversed.join() + "ay";
-
-    pigLatin = pigLatin + currentWordReversed + " ";
+    pigLatin = "$pigLatin${words[i].split("").reversed.join()}ay ";
   }
   return pigLatin;
+}
+
+void main(List<String> args) {
+  String sentence = "The quick brown fox jumps over the lazy dog";
+  print(pigLatin(sentence));
 }
